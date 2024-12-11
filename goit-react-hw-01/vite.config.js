3a0dd5772cbadee7dc4cc318bacc6c82d445234c
JuliaRym <167142301+JuliaRym@users.noreply.github.com>
2024-12-11 03:28:29 +0100

    updates in vite.config and index

--- goit-react-hw-01/vite.config.js
+++ goit-react-hw-01/vite.config.js
@@ -21,17 +21,17 @@
 // import react from "@vitejs/plugin-react";
 
 // export default defineConfig({
+//   root: "./", // Katalog główny, gdzie znajduje się index.html
 //   plugins: [react()],
-//   base: "./",
+//   build: {
+//     outDir: "dist", // Katalog, gdzie będą generowane pliki produkcyjne
+//   },
 // });
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: "./", // Katalog główny, gdzie znajduje się index.html
   plugins: [react()],
-  build: {
-    outDir: "dist", // Katalog, gdzie będą generowane pliki produkcyjne
-  },
+  root: "./",
 });
